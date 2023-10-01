96a6056017bd043cd57e2ef4591f603ea1b9946c
NosovArtem <nosovartem1992@gmail.com>
2023-10-01 20:42:34 +0200

    add metrics

--- src/main/java/com/nsv/soft/dev/lab/spring/example/controller/MyRestController.java
+++ src/main/java/com/nsv/soft/dev/lab/spring/example/controller/MyRestController.java
@@ -12,23 +12,31 @@ public class MyRestController {
 
 
     @GetMapping("/hello")
-    @Counted(value = "greeting.count", description = "COUNT123")
+    @Counted(value = "greeting.count", description = "@Counted - Count taken to return greeting")
     public String sayHello() {
         return "Hello, World!";
     }
 
+    @GetMapping("/show")
+    @Timed(value = "show.time", description = "@Timed - Time taken to return show")
+    public String showSomething() {
+        return "Show something";
+    }
+
     @PostMapping("/add")
-    @Timed(value = "add.time", description = "Time taken to return greeting")
+    @Timed(value = "add.data", description = "@Timed - addData")
     public ResponseEntity<String> addData(@RequestBody String data) {
         return ResponseEntity.ok("Data added: " + data);
     }
 
     @PutMapping("/update")
+    @Timed(value = "update.data", description = "@Timed - updateData")
     public ResponseEntity<String> updateData(@RequestBody String newData) {
         return ResponseEntity.ok("Data updated to: " + newData);
     }
 
     @DeleteMapping("/delete")
+    @Timed(value = "delete.data", description = "@Timed - deleteData")
     public ResponseEntity<String> deleteData() {
         return ResponseEntity.ok("Data deleted");
     }
